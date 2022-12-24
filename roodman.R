015c7f353bd32fbb8cba4da477ae907bc8e0db1d
Arielle <aeh2196@columbia.edu>
2022-12-24 06:03:26 -0500

    annotate, improve plot, 95% confidence interval doesn't match, consider ex vars

--- roodman.R
+++ roodman.R
@@ -5,76 +5,58 @@ X <- readr::read_csv("data/green_winik_data.csv")
 attach(X)
 N <- nrow(X)
 
-
-
+# varied end dates for measuring follow-up period
 end <- sort(rep(seq(2, 365*4, 1), N))
 
+# transform date columns to date values
 dated <- X %>% mutate(across(ends_with("date"), ~as.Date(., "%m/%d/%Y"), .names = "{col}_"))
 
+# make all the variables needed, and repeat the dataframe for each defined range
 expanded <- do.call("rbind", replicate(length(unique(end)), dated, simplify = FALSE)) %>%
   mutate(end = end,
          laterarr2 = ifelse(is.na(sentdate_) | laterarrdate == "0", 0,
                             (sentdate_ + end) > laterarrdate_))
+
+# filter for unique values for each defined range
+# in order to minimize calculation time in the next step
+# (total "laterarr2" each time the range is increased by a day)
 change_index <- expanded %>% count(end, laterarr2) %>% filter(laterarr2 == 0) %>% group_by(n) %>%
   mutate(id = cur_group_id()) %>%
-  summarize(end = max(end), n = n()) %>% arrange(end)
+  # maybe try using the min instead?  but the min isn't working.
+  # attempt a plot to visualize what this means
+  summarize(end = max(end), min_end = min(end), n = n()) %>% arrange(end)
+
+change_index %>%
+  filter(end < 50) %>%
+  ggplot(aes(x = end, y = n)) + geom_step()
 
 listed <- expanded %>% group_by(end) %>% group_split
-x <- listed[[1]]
 
 library(doParallel)
 registerDoParallel(16)
-before <- Sys.time()
 
-out <- map2(.x = listed[(change_index$end - 1)[1:10]],
-     .y = change_index$n[1:10],
+out <- map2(.x = listed[(change_index$end - 1)], # unique versions of the data given the above
+     .y = change_index$n, # keep track of how many days the cumulative laterarr2 remains the same
      .f = function(x, n) {
   outreg <- with(x, liml(Y = laterarr2, D = toserve, Z = as.factor(calendar),
+                         # consider adding in the exogenous variables with D
                          k = 1,
                          manyweakSE = T, clusterID = clusterid))
-  do.call(bind_cols, lapply(outreg[1:5], as.vector)) %>% replicate(n, ., simplify = F)
+  do.call(bind_cols, lapply(outreg[1:5], as.vector)) %>%
+    # repeat the vari
+    replicate(n, ., simplify = F) # repeat the results for each day it was the same
 }) %>% reduce(bind_rows)
-regressed <- out %>% bind_cols(end = unique(expanded$end)[1:nrow(out)])
+regressed <- out %>%
+  # add back in the associated range of days
+  bind_cols(end = unique(expanded$end)[1:nrow(out)])
 
 after <- Sys.time()
 after-before
 
 
-regressed %>% ggplot(aes(x = end, y = cumsum(point.est))) + geom_line()
-
-out <- lapply(listed[1:10], function(x) {
-  outreg <- with(x, liml(Y = laterarr, D = toserve, Z = as.factor(calendar),
-                         k = 1, # 2SLS
-                         manyweakSE = T, clusterID = clusterid))
-  
-  do.call(bind_cols, c(period = unique(x$end), lapply(outreg[1:5], as.vector)))
-}) %>% reduce(bind_rows)
-
-
-sort(rep(seq(2, 365*4, 1), N))
-varied <- lapply(seq(2, 365*4, 1)[1:10], function(end) {
-  new <- X %>% mutate(across(ends_with("date"), ~as.Date(., "%m/%d/%Y"),
-                             .names = "{col}_as"),
-                      end = end,
-                      laterarr2 = ifelse(is.na(sentdate_as) | laterarrdate == "0", 0,
-                              (sentdate_as + end) > laterarrdate_as))
-  ## check output
-  #new %>%
-  #  mutate(range = sentdate_as + end, test = range > laterarrdate_as) %>%
-  #  count(laterarr, laterarr2, laterarrdate_as,
-  #              range, test, sentdate_as, end)
-  dummy <- capture.output(ivmodel)
-  before <- Sys.time()
-  outreg <- with(new, capture.output(ivmodel(Y = laterarr, D = toserve, Z = as.factor(calendar),
-                              k = 1, # 2SLS
-                              manyweakSE = T, clusterID = clusterid)))
-  after <- Sys.time()
-  print(after - before)
-  
-  #do.call(bind_cols, c(period = end,
-  #                     lapply(outreg$LIML[1:5], as.vector)))
-}) #%>% reduce(bind_rows)
-after <- Sys.time()
-
-print(glue::glue("{after - before}"))
-
+regressed %>% ggplot() +
+  geom_path(aes(x = end, y = point.est,
+                size = point.est + 1.960*4*std.err),
+            alpha = .5, color = "lightgreen") +
+  geom_line(aes(x = end, y = point.est), color = "darkgreen") +
+  geom_hline(aes(yintercept = 0))
