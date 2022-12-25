3e1685145316ce3ad7d64e50213b71c00eb06a7b
Arielle <aeh2196@columbia.edu>
2022-12-25 16:38:39 -0500

    experiment with measure for harshness

--- start_up.R
+++ start_up.R
@@ -6,11 +6,30 @@ source("functions/liml.R")
 
 X <- readr::read_csv("data/green_winik_data.csv") %>%
   mutate(across(ends_with("date"), ~as.Date(., "%m/%d/%Y"), .names = "{col}_"))
+
 attach(X)
 N <- nrow(X)
 
 exogenous <- cbind(age, agesq, female, nonblack, priorarr, priordrugarr, priorfelarr, priorfeldrugarr,
                    priorcon, priordrugcon, priorfelcon, priorfeldrugcon, pwid, dist,
                    marijuana, cocaine, crack, heroin, pcp, otherdrug, nondrug)
+
+endogenous <- c("probat", "toserve", "incarcerate", "probatnonzero", "incarc")
+
+# harshness as:
+# 1. toserve
+# 2. toserve/incaceration, if incarceration were predetermined, but not suspension, but bad if probation is less likely
+
+rank_harshness <- function(m) {
+  sym_m <- sym(m)
+  X %>% group_by(calendar) %>% summarize(across(c("probat", "toserve"), ~mean(.))) %>%
+    arrange(!!sym_m)
+}
+
+rank_toserve <- match(calendar, rank_harshness("toserve")$calendar)
+rank_probat <- match(calendar, rank_harshness("probat")$calendar)
+
+X %>% filter(probat+toserve != incarc) %>%
+  select(probat,toserve, incarc)
 
 theme_set(ggthemes::theme_tufte(ticks = F))
