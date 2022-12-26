8c765fb452e82986c5d908d96d9e96e847bed09a
Arielle <aeh2196@columbia.edu>
2022-12-26 07:32:23 -0500

    rm table1 and extract ggridges to expansion

--- green_winik_replication.R
+++ green_winik_replication.R
@@ -1,34 +1,5 @@
 # read in data
 source("start_up.R")
-#### table 1 ####
-#recodes variables and tabulates probation level by prison level
-
-probatlevel <-rep(NA,nrow(X)) 
-# incjudge: indicator judge to be included in analysis
-probatlevel[(incjudge == 1)] <- 0
-# probat: probationary period in months
-probatlevel[((probat > 0 & probat <= 12 & incjudge == 1))] <- 1
-probatlevel[((probat > 12 & probat <= 24 & incjudge == 1))] <- 2
-probatlevel[((probat > 24 & probat <= 36 & incjudge == 1))] <- 3
-probatlevel[((probat > 36 & incjudge == 1))] <- 4
-
-table(probatlevel)/N
-
-prisonlevel <-rep(NA,nrow(X))
-prisonlevel[(incjudge == 1)] <- 0
-# toserve: Non-suspended period of incarceration, in months
-prisonlevel[((toserve > 0 & toserve <= 12 & incjudge == 1))] <- 1
-prisonlevel[((toserve > 12 & toserve <= 24 & incjudge == 1))] <- 2
-prisonlevel[((toserve > 24 & toserve <= 36 & incjudge == 1))] <- 3
-prisonlevel[((toserve > 36 & incjudge == 1))] <- 4
-
-table(prisonlevel)/N
-
-# Table 1:
-table1 = table(probatlevel,prisonlevel)
-table1 = cbind(t, apply(t, 1, sum))
-table1 = rbind(t, apply(t, 2, sum))
-table1
 
 #### table 2 ####
 #summarizes exogenous variables overall, and by calendar
@@ -48,12 +19,6 @@ table_2_reg <- lapply(setNames(colnames(exogenous), colnames(exogenous)), functi
     return(list(reg = reg, chi = chi))
 })
 
-lapply(table_2_reg, function(var) var$chi)
-
-
-#chisq.test(X %>% select(agesq), simulate.p.value = T, B = 1000)$p.value
-# age, agesq, nonblack, check dist. later
-
 #### table 3 ####
 # table 3 #/  ## summarizes endogenous variables by calendar
 endogenous <- c("toserve", "incarc", "incarcerate", "toserve", "probat", "probatnonzero")
@@ -62,29 +27,10 @@ table3 <- X %>% select(calendar, all_of(endogenous)) %>%
     janitor::row_to_names(1, remove_rows_above = FALSE)
 
 table3 <- bind_cols(endogenous = rownames(table3), as_tibble(table3))
-table3
+kableExtra::kable(table3)
 #with(X, do.call(cbind, tapply(toserve, calendar, function(x) c(M = mean(x), SD = sd(x)))))
 #group_by(X, calendar) %>% summarize(M = mean(toserve), SD = sd(toserve))
 
-en_vars <- c("toserve", "probat", "incarc")
-X %>%
-  group_by(calendar) %>%
-  mutate(across(all_of(en_vars), mean, .names = "{col}_mean")) %>%
-  select(calendar, all_of(en_vars), ends_with("_mean")) %>%
-  pivot_longer(all_of(en_vars), names_to = "sentence", values_to = "length") %>%
-  ggplot() +
-  ggridges::geom_density_ridges(
-    aes(x = length, y = reorder(as.factor(calendar), toserve_mean), fill = sentence),
-    scale = 1.2, alpha = 0.50, quantile_lines = T, quantile_fun = median) +
-  xlim(0, NA) +
-  facet_grid(. ~ sentence, scales = "free_x", space = "free_x") +
-  scale_x_continuous(n.breaks = 4, limits = c(0, NA)) +
-  theme(legend.position = "none") +
-  ylab("Calendar\n") + xlab("\nLength") + ggtitle("Distribution of Continuous Treatment Variables") +
-  geom_vline(aes(xintercept = 50))
-
-
-  
 #### table 4 ####
 ## regressions and tests of linear hypotheses of outcomes on covariates with some different empirical specifications (all Ordinary Least Squares)
 covariates <- paste(colnames(exogenous), collapse = ' + ')
@@ -110,11 +56,12 @@ table4 <- bind_rows(lapply(c("incarcerate", "toserve", "probatnonzero", "probat"
           lapply(c("incarcerate", "toserve", "probatnonzero", "probat"), function(x) ftest(x, cov = covariates)$output))
 table4
 
-ftest("toserve")$summary
-summary(lm(formula(glue::glue("toserve ~ {calendars}")), X))$coef %>% as_tibble %>% mutate(calendar = 1:9) %>%
-  arrange(Estimate)
+#ftest("toserve")$summary
+#summary(lm(formula(glue::glue("toserve ~ {calendars}")), X))$coef %>% as_tibble %>% mutate(calendar = 1:9) %>%
+#  arrange(Estimate)
 # weak instrument
 
+#### table 5 ####
 # table 5 #/  ## instrumental variables estimation -- vector of calendar dummies are instruments; toserve and sometimes probat are endogenous explanatory variables, depending on specifications; robust standard errors are clustered on the clusterid var.  
 ## error: unexpected symbol in "ivreg2 laterarr"
 
@@ -143,8 +90,11 @@ ivregs <- lapply(setNames(formulas, formulas),
                 cse = cse))
 })
 
-stargazer::stargazer(ivregs$toserve$plm_cov, output = "outreg_tsls_laterarr_toserve.png")
+table5 <- ivregs$toserve$plm_cov
+stargazer::stargazer(table5, output = "outreg_tsls_laterarr_toserve.png")
 
+
+#### table 7 ####
 # table 7 #/  ## instrumental variables estimation, estimated using limited information maximum likelihood, with robust standard errors clustered on clusterid var.
 ## look into ivmodel package
 
@@ -167,5 +117,6 @@ ivregress <- function(endogenous, outcome = laterarr){
     
     }) %>% reduce(bind_rows)
 }
-endogenous <- c("probat", "toserve", "incarcerate", "probatnonzero")
 table7 <- ivregress("toserve")
+
+
